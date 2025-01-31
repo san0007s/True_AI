a5076b00aa8cdcbe4d5bc44b230e9ad1911addf4
san0007s <149048722+san0007s@users.noreply.github.com>
2025-01-31 12:20:36 +0530

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="Download (1).png">
+        <img src="Download.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
